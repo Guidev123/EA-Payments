298c1a9e3a4331f97dc24c540f925dc00280d013
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-01-20 00:08:03 -0300

    tests: fazendo testes de arquitetura limpa

---
+++ tests/ArchitectureTests/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Payments.Application;
+using Payments.Domain.DomainObjects;
+using Payments.Infrastructure;
+using System.Reflection;
+
+namespace ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(Entity).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ApplicationModule).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(InfrastructureModule).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+}
